43c1fe7d17735e836df8ecb78d5b25ccdbc161ae
latinosmuchachos <89702294+latinosmuchachos@users.noreply.github.com>
2023-04-06 11:02:58 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,7 +3,8 @@ import numpy as np
 
 
 chat_id = 649103283 # Ваш chat ID, не меняйте название переменной
+t = 17
 
 def solution(x: np.array) -> float:
     
-    return x.mean() / 17 / 17 # Ваш ответ
+    return 2 * x.mean() / t**2 # Ваш ответ
